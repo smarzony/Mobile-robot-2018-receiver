e3f2484bc9344acc5844494964625763434c1dd0
Piotr Smarzynski <smarzony@gmail.com>
2018-11-05 22:19:21 +0100

    extended radio setup

--- Mobile Robot 2018 receiver/Mobile Robot 2018 receiver.ino
+++ Mobile Robot 2018 receiver/Mobile Robot 2018 receiver.ino
@@ -5,28 +5,55 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(26,27);
+#define ONE_MS 1
 
-const byte rxAddr[6] = "00001";
+RF24 radio(26, 27);
+
+const byte rxAddr[6] = {'1','N','o','d','e','0'};
+byte incoming_message[10];
+
+unsigned long long now, last_message_read;
 
 void setup()
 {
-	while (!Serial);
 	Serial.begin(9600);
 
 	radio.begin();
+	radio.setChannel(110);
 	radio.openReadingPipe(0, rxAddr);
-
 	radio.startListening();
 }
 
 void loop()
 {
+	now = millis();
+	//readRadio(ONE_MS);
 	if (radio.available())
 	{
-		char text[32];
-		radio.read(&text, sizeof(text));
-
-		Serial.println(text);
+		radio.read(&incoming_message, sizeof(incoming_message));
+		printMessage();
 	}
 }
+
+void readRadio(int period)
+{
+	if (now - last_message_read >= period)
+	{
+		if (radio.available())
+		{
+			radio.read(&incoming_message, sizeof(incoming_message));
+			printMessage();
+		}
+	}
+}
+
+void printMessage()
+{
+	for (int i = 0; i < sizeof(incoming_message); i++)
+	{
+		Serial.print(incoming_message[i]);
+		Serial.print(' ');
+	}
+	Serial.println();
+}
+
